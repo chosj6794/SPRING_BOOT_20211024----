370f069e356eef164ad14288fe0a854f8e4cfe4f
admin <chosj4833@naver.com>
2024-10-20 22:28:43 +0900

    6주차 블로그 게시판 – 2 완료

--- src/main/java/com/example/demo/model/service/BlogService.java
+++ src/main/java/com/example/demo/model/service/BlogService.java
@@ -1,5 +1,7 @@
 package com.example.demo.model.service;
+
 import java.util.List;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.example.demo.model.domain.Article;
@@ -7,22 +9,31 @@ import com.example.demo.model.repository.BlogRepository;
 import lombok.RequiredArgsConstructor;
 
 @Service
-@RequiredArgsConstructor // 생성자 자동 생성(부분)
+@RequiredArgsConstructor // 생성자 자동 생성
 public class BlogService {
-@Autowired // 객체 주입 자동화, 생성자 1개면 생략 가능
-private final BlogRepository blogRepository; // 리포지토리 선언
+    private final BlogRepository blogRepository; // 리포지토리 선언
 
-public List<Article> findAll() { // 게시판 전체 목록 조회
-return blogRepository.findAll();
-}
+    public List<Article> findAll() { // 게시판 전체 목록 조회
+        return blogRepository.findAll();
+    }
 
-public Article save(AddArticleRequest request){
-    // DTO가 없는 경우 이곳에 직접 구현 가능
-    // public ResponseEntity<Article> addArticle(@RequestParam String title, @RequestParam String content) {
-    // Article article = Article.builder()
-    // .title(title)
-    // .content(content)
-    // .build();
-    return blogRepository.save(request.toEntity());
+    public Article save(AddArticleRequest request) {
+        return blogRepository.save(request.toEntity()); // DTO로부터 Article 엔티티 생성 후 저장
+    }
+
+    public Optional<Article> findById(Long id) { // 게시판 특정 글 조회
+        return blogRepository.findById(id);
+    }
+
+    public void update(Long id, AddArticleRequest request) {
+        Optional<Article> optionalArticle = blogRepository.findById(id); // 단일 글 조회
+        optionalArticle.ifPresent(article -> { // 값이 있으면
+            article.update(request.getTitle(), request.getContent()); // 값을 수정
+            blogRepository.save(article); // 수정된 Article 객체 저장
+        });
+    }
+
+    public void delete(Long id) { // 게시글 삭제
+        blogRepository.deleteById(id); // ID로 게시글 삭제
     }
 }
